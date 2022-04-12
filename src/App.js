a5620f45873bbb4974f5421a10cbcf2d24bce49d
Kipouralkis <38573852+Kipouralkis@users.noreply.github.com>
2022-04-12 13:16:59 +0300

    fixed searchbar dependencies

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ import ForRent from "./Pages/ForRent";
 import HousePage from "./Pages/HousePage";
 import Filter from "./components/filter/filter";
 
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
+
 
 
 function App() {
